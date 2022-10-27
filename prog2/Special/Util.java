be754e49093c774439e0857d9ae681fa6b4c6077
Quinn York <qyork@ncsu.edu>
2022-10-27 11:41:03 -0500

    added prog2 folder

---
+++ prog2/Special/Util.java
@@ -0,0 +1,38 @@
+// Util -- Utility functions
+
+package Special;
+
+import Tree.Node;
+import Tree.Nil;
+import Tree.Cons;
+import Tree.Environment;
+
+public class Util {
+    // length returns the length of a well-formed list exp and -1 otherwise
+    public static int length(Node exp) {
+        if (exp.isNull())
+            return 0;
+        if (!exp.isPair())
+            return -1;
+        int n = length(exp.getCdr());
+        if (n == -1)
+            return -1;
+        return n + 1;
+    }
+
+    // mapeval calls eval on every list element of exp
+    public static Node mapeval(Node exp, Environment env) {
+        if (exp.isNull())
+            return Nil.getInstance();
+        return new Cons(exp.getCar().eval(env), mapeval(exp.getCdr(), env));
+    }
+
+    // begin calls eval on all list elements and returns the last value
+    public static Node begin(Node exp, Environment env) {
+        Node res = exp.getCar().eval(env);
+        Node cdr = exp.getCdr();
+        if (cdr.isNull())
+            return res;
+        return begin(cdr, env);
+    }
+}
